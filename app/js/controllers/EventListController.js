be9c3922962cca0bc9f85f2635e2a42a1a2882bc
Daniel Cantau <daniel.cantau.ext@degremont.com>
2016-08-15 17:21:29 +0200

    Testing EventListController

--- app/js/controllers/EventListController.js
+++ app/js/controllers/EventListController.js
@@ -1,3 +1,3 @@
-eventsApp.controller('EventListController', function($scope, $location, eventData) {
+eventsApp.controller('EventListController', function($scope, eventData) {
     $scope.events = eventData.getAllEvents();
 });
