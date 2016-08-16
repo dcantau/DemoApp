8d193d64ed78b3ccfe73f9d4f007168ac30dbdc9
Daniel Cantau <daniel.cantau.ext@degremont.com>
2016-08-16 17:19:22 +0200

    Added eventThumbnail directive

---
+++ app/js/directives/eventThumbnail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+eventsApp.directive('eventThumbnail', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/templates/directives/eventThumbnail.html',
+        replace: true
+    }
+});
